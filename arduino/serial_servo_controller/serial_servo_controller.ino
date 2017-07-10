1a5464742d1b9e5f6e2d7fb04114a2b81526eee0
Mario <seelim@student.ethz.ch>
2017-07-10 20:59:57 +0200

    unterstuetzung 3 servo boards

--- arduino/serial_servo_controller/serial_servo_controller.ino
+++ arduino/serial_servo_controller/serial_servo_controller.ino
@@ -2,7 +2,9 @@
 #include <Adafruit_PWMServoDriver.h>
 
 
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x42);
+Adafruit_PWMServoDriver pwm0 = Adafruit_PWMServoDriver(0x40);
+Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x41);
+Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x42);
 
 int pulsewidth(float angle){
   float pw_ms = (angle + 78.75)/112.5;
@@ -12,10 +14,17 @@ int pulsewidth(float angle){
 void setup() {  
   Serial.begin(9600);
 
-  pwm.begin();
-  pwm.setPWMFreq(55.55555);  // 55.55 * 0.9 = 50 Hz
+  pwm0.begin();
+  pwm0.setPWMFreq(55.55555);  // 55.55 * 0.9 = 50 Hz
+  pwm0.setPin(0, pulsewidth(90)); // 307 = 4096 / 20ms * 1.5ms
 
-  pwm.setPin(0, pulsewidth(90)); // 307 = 4096 / 20ms * 1.5ms
+  pwm1.begin();
+  pwm1.setPWMFreq(55.55555);  // 55.55 * 0.9 = 50 Hz
+  pwm1.setPin(0, pulsewidth(90)); // 307 = 4096 / 20ms * 1.5ms
+
+  pwm2.begin();
+  pwm2.setPWMFreq(55.55555);  // 55.55 * 0.9 = 50 Hz
+  pwm2.setPin(0, pulsewidth(90)); // 307 = 4096 / 20ms * 1.5ms
   
 }
 
@@ -28,9 +37,21 @@ void loop() {
       int angle = Serial.read();
       int board = servo >> 4;
       servo = servo & 0x0F;
+      //Serial.print("Board: ");
       //Serial.print(board);
+      //Serial.print(" Servo: ");
+      //Serial.print(servo);
+      //Serial.print(" Angle: ");
       //Serial.println(angle);
-      pwm.setPin(servo, pulsewidth(angle)); // 307 = 4096 / 20ms * 1.5ms
+      if(board == 0){
+        pwm0.setPin(servo, pulsewidth(angle)); // 307 = 4096 / 20ms * 1.5ms
+        }
+      else if(board == 1){
+        pwm0.setPin(servo, pulsewidth(angle)); // 307 = 4096 / 20ms * 1.5ms
+        }
+      else if(board == 2){
+        pwm2.setPin(servo, pulsewidth(angle)); // 307 = 4096 / 20ms * 1.5ms
+        }
     }
     delay(10);
     Serial.flush();
